6b010ee63f3ea53d58909324cd553b32c99f0d08
Kyle Gendreau <kyle@Kyles-MacBook-Pro.local>
2023-02-20 11:54:44 -0600

    for example

---
+++ src/exampleComponent.tsx
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+import { Button } from '@mui/material';
+
+function ExampleComponent() {
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  );
+};
+
+export default ExampleComponent;
