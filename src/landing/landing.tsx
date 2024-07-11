9d70a1a936fda7d70f8fd775207d12b9c3b62c7d
Kyle <47574000+kylegendy@users.noreply.github.com>
2024-07-11 11:19:35 -0400

    added material-tailwind and removed materialui due to conflicts between mui and tailwind

--- src/landing/landing.tsx
+++ src/landing/landing.tsx
@@ -1,47 +1,25 @@
 import './landing.css'
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@material-tailwind/react";
 
 import ExampleComponent from "./exampleComponent";
 
 // import PouchDB from 'pouchdb';
 // const db: PouchDB.Database<{}> = new PouchDB('blob');
 
-const theme = createTheme({
-    typography: {
-        fontFamily: ["Montserrat", "Open Sans", "Vidaloka", "sans-serif"].join(','),
-    },
-    palette: {
-        primary: { main: "#9C9989", dark: "#726E58" },
-        secondary: { main: "#A3D6F4" },
-        success: { main: "#6B8F71" },
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-                @font-face {
-                    font-family: 'Montserrat';
-                    font-style: normal;
-                    font-display: swap;
-                    font-weight: 400;
-                }
-            `,
-        },
-    }
-});
 
 function Landing() {
     return (
-        <ThemeProvider theme={theme}>
-            <ExampleComponent />
-        </ThemeProvider>
+        <ExampleComponent />
     );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <Landing />
-    </React.StrictMode>
+    <ThemeProvider>
+        <React.StrictMode>
+            <Landing />
+        </React.StrictMode>
+    </ThemeProvider>
 );
